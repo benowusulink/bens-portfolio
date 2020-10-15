ac0253dfe5752e01835fe22a65b70017085a977e
benzotest <benzo120test@outlook.com>
2020-10-15 23:54:44 +0100

    nearly finished home page

---
+++ src/components/Navbar1/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./Navbar.css";
+import logo from "./assets/logo.png";
+
+class Navbar1 extends Component {
+  render() {
+    return (
+      <nav>
+        <img src={logo} alt={`logo`} className={`logo-image`} />
+        <ul>
+          <li>
+            <p>who i am</p>
+          </li>
+          <li>
+            <p>projects</p>
+          </li>
+          <li>
+            <p>what i know</p>
+          </li>
+          <li>
+            <p>contact</p>
+          </li>
+          <li>
+            <a
+              href="https://github.com/benowusulink"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ion-icon name="logo-github"></ion-icon>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar1;
