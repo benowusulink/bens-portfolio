478f06b86291e5ed1ba43314fcd7863098054dc1
benzotest <benzo120test@outlook.com>
2020-10-15 15:07:06 +0100

    starting project

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Navbar.css";
+
+class Navbar extends Component {
+  render() {
+    return <nav>hiiiiii</nav>;
+  }
+}
+
+export default Navbar;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "../components/Navbar.js";
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className={`app`}>
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
